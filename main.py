2a4443e50f265df964897c5ece40baa5dd89ee4f
Ben Secor <bsecor@bowdoin.edu>
2024-02-21 13:29:54 -0500

    add cool and finish video

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lexer import LexToken
 import yacc as yacc
 
 tokens_filename = sys.argv[1]
-tokens_filehandle = open(tokens_filename, "r")
+tokens_filehandle = open(tokens_filename, 'r')
 tokens_lines = tokens_filehandle.readlines()
 tokens_filehandle.close()
 
@@ -93,12 +93,12 @@ tokens = (
 )
 
 def p_program_classlist(p):
-    'program : class_list'
+    'program : classlist'
     p[0] = p[1]
 
 def p_classlist_one(p):
-    'class_list : class SEMI'
-    p[0] = [p[1]]
+    'classlist : '
+    p[0] = []
 
 def p_classlist_some(p):
     'classlist : class SEMI classlist'
@@ -112,6 +112,10 @@ def p_type(p):
     'type : TYPE'
     p[0] = (p.lineno(1), p[1])
 
+def p_indentifier(p):
+    'identifier : IDENTIFIER'
+    p[0] = (p.lineno(1), p[1])
+
 def p_featurelist_none(p):
     'featurelist :'
     p[0] = []
@@ -119,3 +123,51 @@ def p_featurelist_none(p):
 def p_featurelist_some(p):
     'featurelist : feature SEMI featurelist'
     p[0] = [p[1]] + p[3]
+
+def p_feature_attributenoinit(p):
+    'feature : identifier COLON type'
+    p[0] = (p.lineno(1), 'attribute_no_init', p[1], p[3])
+
+def p_error(p):
+    if p:
+         print("Error: ", p.lineno, ": Parser:  parse error near ", p.type)
+         # Just discard the token and tell the parser it's okay.
+         exit(1)
+    else:
+        #  FIGUREOUT HOW TO END FILE
+         print("Syntax error at EOF") #FIX THIS
+
+#build the PA3 parser from the above rules 
+
+parser = yacc.yacc()
+ast = yacc.parse(lexer=pa2lexer)
+
+print(ast)
+
+ast_filename = (sys.argv[1])[:-4]+ "-ast"
+fout = open(ast_filename, 'w')
+
+def print_identifier(ast):
+    fout.write(str(ast[0]) + "\n")
+    fout.write(ast[1] + "\n")
+
+def print_feature(ast):
+    fout.write("attribute_no_init\n")
+    print_identifier(ast[2])
+    print_identifier(ast[3])
+
+def print_list(ast, print_element_function):
+    fout.write(str(len(ast))+ "\n") 
+    for elem in ast:
+        print_element_function(elem)
+
+def print_class(ast):
+    print_identifier(ast[2])
+    fout.write("no_inherits\n")
+    print_list(ast[3], print_feature)
+
+def print_program(ast):
+    print_list(ast, print_class)
+    
+print_program(ast)
+fout.close()
